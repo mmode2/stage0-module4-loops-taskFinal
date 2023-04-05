08ae97c36a204150771afd64d1b4a9733e91f060
Maiko <maiko.mode05@gmail.com>
2023-04-05 09:42:13 +0400

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,16 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
+        if (numberToPrint == 0) {
+            System.out.println(0);
+            return;
+        }
+        int result = 1;
+        for (int i = 0; i < power; i++) {
+            result *= numberToPrint;
 
+        }
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,6 +3,26 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
 
+
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int k = cathetusLength; k >= 1; k--) {
+                if (k > i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print(k);
+                }
+            }
+            for (int k = 2; k <= cathetusLength; k++) {
+                if (k <= i) {
+                    System.out.print(k);
+                }
+            }
+
+            System.out.println();
+
+        }
+
+
     }
 
     public static void main(String[] args) {
